4197a1269c34a86e471a7531a844445509e32622
Mateen Kazia <mateencode@gmail.com>
2020-02-05 21:49:02 -0600

    textfield type checking

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import React from "react";
+import { TextField } from "./components/TextField";
+
 import "./App.css";
 
 const App = () => {
-  return <div className='App'></div>;
+  return (
+    <div className='App'>
+      <TextField text='hello' />
+    </div>
+  );
 };
 
 export default App;
---
+++ src/components/TextField.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+interface Props {
+  text: string;
+}
+
+export const TextField: React.FC<Props> = () => (
+  <div>
+    <input />
+  </div>
+);
